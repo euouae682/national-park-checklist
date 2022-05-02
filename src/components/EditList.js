3f7223b9aded0ec1bcc72d5e685ba045682a5b1d
euouae682 <jasonuzhao682@gmail.com>
2022-05-01 18:38:47 -0700

    set up basic json-server

--- src/components/EditList.js
+++ src/components/EditList.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export const EditList = () => {
+  const toggleEdit = () => {
+    console.log("this sorts the parks");
+  }
+
   return (
     <aside className="editlist">
       <h1>
